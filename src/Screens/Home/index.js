8a31829c331fec8ebc2889028b3c5674f0e2da12
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-06-21 16:35:57 -0300

    inserção de components

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import { ScrollView, StyleSheet } from 'react-native';
+import Endereco from '../../Components/Home/Endereco';
+import Input from '../../Components/Input';
+import CupomDesconto from '../../Components/Home/CupomDesconto';
+import Sugestoes from '../../Components/Home/Sugestoes';
+import Promocoes from '../../Components/Home/Promocoes';
+import Ofertas from '../../Components/Home/Ofertas';
+import Categorias from '../../Components/Home/Categorias';
+import Restaurantes from '../../Components/Home/Restaurantes';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <ScrollView showsHorizontalScrollIndicator={true} style={styles.container}>
+      <Endereco />
+      <Input placeholder="Busque por item ou loja" />
+      <CupomDesconto />
+      <Sugestoes />
+      <Promocoes />
+      <Ofertas />
+      <Categorias />
+      <Restaurantes />
+    </ScrollView>
   );
 }
 
@@ -13,7 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
